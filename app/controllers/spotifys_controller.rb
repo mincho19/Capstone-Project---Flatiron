5603f2de2da72da717f787e3a901642198dd3301
Min Cho <minsangcho19@gmail.com>
2022-03-19 12:23:20 -0700

    I GOT ACCESS TO DATA

--- app/controllers/spotifys_controller.rb
+++ app/controllers/spotifys_controller.rb
@@ -1,16 +1,11 @@
 class SpotifysController < ApplicationController
-
-    user = User.first
-    headers = {Authentication: "Bearer #{user.access_token}"}
-    puts User.all
-
-    # how do i access session?
-
+    
     def getTopTracks
+        user = User.find_by(id: session[:user_id])
         time = params[:time_range]
         header = {Authorization: "Bearer #{user["access_token"]}"}
         tracks_response = RestClient.get("https://api.spotify.com/v1/me/top/tracks?time_range=#{time}&limit=20", header)
-        tracks_parms = JSON.parse(tracks_response.body)
+        render json: tracks_response
     end
 
 end
