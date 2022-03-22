7d3443d12d5c42be6ab7f63ada954af657e4d840
Min Cho <minsangcho19@gmail.com>
2022-03-21 21:31:08 -0700

    artist and albums done

--- client/src/Main.js
+++ client/src/Main.js
@@ -22,7 +22,7 @@ export default function Main() {
   function fetchTopTracks(time, limit){
     fetch(`/spotify/top/tracks/${time}/${limit}`)
     .then((res) => res.json())
-    .then((data) => setTopSongs(data));
+    .then((data) => console.log(data))
   }
 
   function resetBackground(){
